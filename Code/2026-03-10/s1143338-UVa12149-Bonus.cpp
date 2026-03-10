ed1d856e14cb43f3b96ea423aafd9890b62fb414
JuiChenWu <s1143338@mail.yzu.edu.tw>
2026-03-10 11:40:45 +0800

    Add files via upload

---
+++ Code/2026-03-10/s1143338-UVa12149-Bonus.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+
+#define int long long
+using namespace std;
+
+inline int read(){
+    int x=0;
+    char c=0;
+    while(c<'0' || c>'9'){
+        c=getchar();
+        if(c==-1){return 0;}
+    }
+    while(c>='0'&&c<='9'){
+        x=(x<<3)+(x<<1)+c-'0';
+        c=getchar();
+    }
+    return x;
+}
+inline void write(int x){
+    if(x>=10){write(x/10);}
+    putchar(x%10+'0');
+}
+
+signed main(){
+    int N;
+    while(N=read()){
+        write(N*(N+1)*(N*2+1)/6);
+        putchar('\n');
+    }
+    return 0;
+}
